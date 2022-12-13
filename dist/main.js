b2da4343d75e90a7e41cf551bb955f093640083f
ademesojosiah <josiahademeso@gmail.com>
2022-12-13 06:25:29 +0100

    type insertion

--- dist/main.js
+++ dist/main.js
@@ -1,10 +1,20 @@
+var pageNumber = '1';
+var NumericPageNumber = pageNumber;
 // const getFullName = (firstname:string,lastname:string):string =>{
 //     return firstname + "" + lastname
 // }
-var user1 = {
-    name: 'josiah',
-    age: 20,
-    "class": ['jojo', 'ade']
-};
-var prices = [4, 5, 6, 7];
-var myNumber = null;
+// console.log(getFullName('jojo','ade'));
+// type ID = number
+// type maybeNumber = ID | null
+// interface UserInterface {
+//     name:string,
+//     age:number,
+//     class:string[]
+// }
+// const user1:UserInterface = {
+//     name:'josiah',
+//     age:20,
+//     class:['jojo','ade']
+// }
+// let prices:ID[] = [4,5,6,7]
+// let myNumber:maybeNumber = 80
--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,33 @@
+let pageNumber:string = '1'
+let NumericPageNumber:number = (pageNumber as unknown ) as number
+
+
+
+
+
+
+
 // const getFullName = (firstname:string,lastname:string):string =>{
 //     return firstname + "" + lastname
 // }
 
 // console.log(getFullName('jojo','ade'));
-type ID = number
-type maybeNumber = number | null
+// type ID = number
+// type maybeNumber = ID | null
 
 
-interface UserInterface {
-    name:string,
-    age:number,
-    class:string[]
-}
+// interface UserInterface {
+//     name:string,
+//     age:number,
+//     class:string[]
+// }
 
 
-const user1:UserInterface = {
-    name:'josiah',
-    age:20,
-    class:['jojo','ade']
-}
+// const user1:UserInterface = {
+//     name:'josiah',
+//     age:20,
+//     class:['jojo','ade']
+// }
 
-let prices:ID[] = [4,5,6,7]
-let myNumber:maybeNumber = null
+// let prices:ID[] = [4,5,6,7]
+// let myNumber:maybeNumber = 80
